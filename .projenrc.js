82822857f7a2c80a1259f9f0ef69ded9d365268c
ghawker <ghawker@schubergphilis.com>
2022-02-07 20:59:02 +0100

    release workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,13 +1,22 @@
 const { awscdk } = require("projen");
+const { DependabotScheduleInterval } = require("projen/lib/github");
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: "2.1.0",
   defaultReleaseBranch: "main",
+  description: "Testing out projen",
+  dependabot: true,
+  dependabotOptions: {
+    autoMerge: true,
+    ignoreProjen: true,
+    scheduleInterval: DependabotScheduleInterval.WEEKLY,
+  },
   deps: ["aws-cdk-lib", "cdk-dynamo-table-viewer", "constructs"],
+  // devDeps: [],             /* Build dependencies for this module. */
   gitignore: ["*/.idea/*"],
   name: "projen",
   prettier: true,
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
+  releaseWorkflow: true,
+
   // packageName: undefined,  /* The "name" in package.json. */
 });
 project.synth();
